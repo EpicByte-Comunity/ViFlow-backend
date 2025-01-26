a4028c75c0abba5afe8e015d61f2a4c9c17556ed
Kevin Villegas <kevinvilleperez@gmail.com>
2025-01-26 02:35:36 -0500

    refactor: configurate and docker .env

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -9,7 +9,7 @@ export const databaseProviders = [
       const dataSource = new DataSource({
         type: 'postgres',
         host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT', 3306),
+        port: configService.get<number>('DB_PORT', 5432),
         username: configService.get<string>('DB_USERNAME'),
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_DATABASE'),
